cbc362e1a9cde3c6368df0e9d1012a5980c0b792
Lars Kellogg-Stedman <lars@redhat.com>
2020-10-15 14:20:43 -0400

    initial import

---
+++ README.md
@@ -0,0 +1,48 @@
+# OBCAWS
+
+A wrapper script for running `aws-cli` commands against object buckets in
+[OpenShift Container Storage][ocs] (OCS).
+
+[ocs]: https://www.redhat.com/en/technologies/cloud-computing/openshift-container-storage
+
+## Requirements
+
+- This script uses `podman` to run the `amazon/aws-cli` image, so you'll need
+  to have [podman][] installed.
+
+- We use [jq][] for parsing JSON output from `kubectl get ...`. You'll need
+  something relatively recent (at least 1.6) because we make use of the
+  `base64d` filter to decode base64-encoded data.
+
+[podman]: https://podman.io/
+[jq]: https://stedolan.github.io/jq/
+
+## Usage
+
+```
+obcaws: usage: obcaws [ -n namespace ] [ -r namespace/route ]
+  [-v] [-h bucket_host]
+```
+
+For example, if I want to list objects in the bucket associated with my
+`images` `ObjectBucketClaim` in my current namespace, using the external route
+provided by the `s3` route in the `openshift-storage` namespace, I can run:
+
+```
+obsaws -r openshift-storage/s3 images s3 ls BUCKET
+```
+
+The token `BUCKET` will be replaced with the bucket name from the `OBC`.
+The above command will get translated into something like:
+
+```
+podman run --rm \
+  -v .awsoRs3VH:/root/.aws \
+  -v $PWD:$PWD -w $PWD \
+  amazon/aws-cli s3 --endpoint https://s3-openshift-storage.apps.example.com \
+  ls images-eb819852-3a1f-4be3-96ec-eada6b6327eb
+```
+
+The first volume in this command line (`-v .awsoRs3VH:/root/.aws`) contains
+AWS-style credentials extracted from the secret that is associated with
+your `OBC` resource.
---
+++ obcaws.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+set -e
+
+loglevel=0
+
+log() {
+	local level=$1
+	shift
+	local msg="$*"
+
+	if [[ $loglevel -ge $level ]]; then
+		echo "$0: $msg" >&2
+	fi
+}
+
+usage() {
+	echo "$0: usage: $0 [ -n namespace ] [ -r namespace/route ] [-v] [-h bucket_host]"
+}
+
+while getopts 'n:h:r:v' ch; do
+	case $ch in
+	(n)	namespace=$OPTARG;;
+	(h)	bucket_host=$OPTARG;;
+	(r)	bucket_host_route=$OPTARG;;
+	(v)	loglevel=$(( loglevel + 1 ));;
+	(--)	break;;
+	(\?)	usage >& 2
+		exit 1
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+
+obc=$1
+api=$2
+shift 2
+
+if [[ -z $bucket_host ]] && [[ $bucket_host_route ]]; then
+	bucket_host_ns=${bucket_host_route%/*}
+	bucket_host_route=${bucket_host_route#*/}
+	bucket_host=$(kubectl -n $bucket_host_ns \
+		get route $bucket_host_route -o json |
+		jq -r '.status.ingress[0].host')
+fi
+
+tempdir=$(mktemp -d .awsXXXXXX)
+trap "rm -rf $tempdir" EXIT
+
+log 1 "getting obc secret"
+kubectl ${namespace:+-n $namespace} get secret $obc -o json > $tempdir/secret.json
+
+log 1 "getting obc config"
+kubectl ${namespace:+-n $namespace} get configmap $obc -o json > $tempdir/config.json
+
+if [[ -z $bucket_host ]]; then
+	bucket_host=$(jq -r .data.BUCKET_HOST $tempdir/config.json)
+fi
+
+bucket_name=$(jq -r .data.BUCKET_NAME $tempdir/config.json)
+aws_access_key_id=$(jq -r '.data.AWS_ACCESS_KEY_ID|@base64d' $tempdir/secret.json)
+aws_secret_access_key=$(jq -r '.data.AWS_SECRET_ACCESS_KEY|@base64d' $tempdir/secret.json)
+endpoint=https://${bucket_host}
+
+log 2 "bucket_host = $bucket_host"
+log 2 "bucket_name = $bucket_name"
+log 2 "endpoint = $endpoint"
+
+
+cat >$tempdir/credentials <<EOF
+[default]
+AWS_ACCESS_KEY_ID=$aws_access_key_id
+AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
+EOF
+
+args=()
+for arg in "$@"; do
+	args+=(${arg/BUCKET/$bucket_name})
+done
+
+podman run --rm \
+	-v $tempdir:/root/.aws \
+	-v $PWD:$PWD -w $PWD \
+	amazon/aws-cli $api --endpoint $endpoint "${args[@]}"
